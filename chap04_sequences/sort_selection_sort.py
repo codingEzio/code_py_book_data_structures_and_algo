0a541cd47d02fcf145550023980a88bf2c822864
Ezio <assassinste@gmail.com>
2020-01-03 22:37:29 +0800

    Added a simple selection sort

---
+++ chap04_sequences/sort_selection_sort.py
@@ -0,0 +1,30 @@
+from pylist import IntList
+
+
+def selection_sort(seq: IntList) -> None:
+    """
+    Sort lists in-place (complexity: O(n²), yikes).
+    Ref: https://djangocentral.com/selection-sort-in-python/
+    """
+
+    start: int
+    idx: int
+
+    for start in range(len(seq)):
+        min_pos = start
+
+        for idx in range(start, len(seq)):
+            if seq[idx] < seq[min_pos]:
+                min_pos = idx
+
+        (seq[start], seq[min_pos]) = (seq[min_pos], seq[start])
+
+
+def main() -> None:
+    arr: IntList = [9, 1, 3]
+    selection_sort(seq=arr)
+    assert arr == [1, 3, 9]
+
+
+if "__main__" == __name__:
+    main()
