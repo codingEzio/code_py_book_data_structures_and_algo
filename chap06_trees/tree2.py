4c3ad62dc3e9b401f62e5f3d937c4325443d78ff
Ezio <assassinste@gmail.com>
2020-01-07 22:36:59 +0800

    Add 'insert' feature: [insert_left, insert_right]

--- chap06_trees/tree2.py
+++ chap06_trees/tree2.py
@@ -7,7 +7,29 @@ class BinaryTree:
         self.left_child = None
         self.right_child = None
 
-    pass
+    def insert_left(self, value):
+        """
+        Either create a new one
+        or     replace the old one with the new one (& new one as its child XD)
+        """
+        if self.left_child is None:
+            self.left_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.left_child = self.left_child
+            self.left_child = new_node
+
+    def insert_right(self, value):
+        """
+        Either create a new one
+        or     replace the old one with the new one (& new one as its child XD)
+        """
+        if self.right_child is None:
+            self.right_child = BinaryTree(value)
+        else:
+            new_node = BinaryTree(value)
+            new_node.right_child = self.right_child
+            self.right_child = new_node
 
 
 def main() -> None:
@@ -15,6 +37,16 @@ def main() -> None:
     assert bi.value == "A"
     assert bi.left_child is None
     assert bi.right_child is None
+
+    bi.insert_left("B")  # A -> B
+    bi.insert_left("C")  # A -> C -> B
+    assert bi.left_child.value == "C"
+    assert bi.left_child.left_child.value == "B"
+
+    bi.insert_right("D")  # A -> D
+    bi.insert_right("E")  # A -> E -> D
+    assert bi.right_child.value == "E"
+    assert bi.right_child.right_child.value == "D"
 
 
 if "__main__" == __name__:
