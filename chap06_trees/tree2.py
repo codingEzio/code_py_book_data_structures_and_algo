a5b4181dce9ae864afdffaf8be230dce337aa724
Ezio <assassinste@gmail.com>
2020-01-08 11:21:13 +0800

    Added different traversal methods & new test code

--- chap06_trees/tree2.py
+++ chap06_trees/tree2.py
@@ -31,22 +31,88 @@ class BinaryTree:
             new_node.right_child = self.right_child
             self.right_child = new_node
 
+    def pre_order(self):
+        """LEFT -> LEFT -> .. THE_REST -> RIGHT -> LEFT -> .. THE_REST"""
+        print(self.value)
+
+        if self.left_child:
+            self.left_child.pre_order()
+
+        if self.right_child:
+            self.right_child.pre_order()
+
+    def in_order(self):
+        """LEFT MIDDLE RIGHT -> BACKTRACK -> LEFT MIDDLE RIGHT"""
+        if self.left_child:
+            self.left_child.in_order()
+
+        print(self.value)
+
+        if self.right_child:
+            self.right_child.in_order()
+
+    def post_order(self):
+        """LEFT RIGHT MIDDLE -> BACKTRACK -> LEFT RIGHT MIDDLE"""
+        if self.left_child:
+            self.left_child.post_order()
+
+        if self.right_child:
+            self.right_child.post_order()
+
+        print(self.value)
+
+
+def are_my_nodes_in_the_right_place(root_node) -> bool:
+    """
+    Expected result:
+          A
+        B   F
+       C E  G H
+      D
+    """
+    lvl2_b = root_node.left_child
+    lvl2_f = root_node.right_child
+
+    assert lvl2_b.value == "B"
+    assert lvl2_f.value == "F"
+
+    assert lvl2_b.left_child.value == "C"
+    assert lvl2_b.right_child.value == "E"
+    assert lvl2_f.left_child.value == "G"
+    assert lvl2_f.right_child.value == "H"
+
+    assert lvl2_b.left_child.left_child.value == "D"
+
 
 def main() -> None:
-    bi = BinaryTree("A")
-    assert bi.value == "A"
-    assert bi.left_child is None
-    assert bi.right_child is None
+    root = BinaryTree("A")
 
-    bi.insert_left("B")  # A -> B
-    bi.insert_left("C")  # A -> C -> B
-    assert bi.left_child.value == "C"
-    assert bi.left_child.left_child.value == "B"
+    # Insert lvl2 node
+    root.insert_left("B")
+    root.insert_right("F")
 
-    bi.insert_right("D")  # A -> D
-    bi.insert_right("E")  # A -> E -> D
-    assert bi.right_child.value == "E"
-    assert bi.right_child.right_child.value == "D"
+    # Insert lvl3 node
+    node_lv2_b = root.left_child  # entry
+    node_lv2_f = root.right_child  # entry
+    node_lv2_b.insert_left("C")
+    node_lv2_b.insert_right("E")
+    node_lv2_f.insert_left("G")
+    node_lv2_f.insert_right("H")
+
+    # Insert lvl4 node (only a leaf-node "D")
+    node_lvl3_c = node_lv2_b.left_child  # entry
+    node_lvl3_c.insert_left("D")
+
+    # Expected result
+    #     A
+    #   B   F
+    #  C E  G H
+    # D
+    are_my_nodes_in_the_right_place(root)
+
+    # root.pre_order()  # Expected: A B C D E F G H
+    # root.in_order()   # Expected: D C B E A G F H
+    # root.post_order() # Expected: D C E B G H F A
 
 
 if "__main__" == __name__:
