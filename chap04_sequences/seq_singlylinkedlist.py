84227e8679f6a456d32b3c7796a93d4f9791debf
Ezio <assassinste@gmail.com>
2021-03-30 19:37:56 +0800

    Heavily documented Singly Linked List implementation (complete, finished)

--- chap04_sequences/seq_singlylinkedlist.py
+++ chap04_sequences/seq_singlylinkedlist.py
@@ -139,20 +139,121 @@ class SinglyLinkedList:
             current_node = current_node.next
         current_node.next = new_node
 
+    def insert_before(self, index, value):
+        """
+        O(N)
+        """
+        if index < 0:
+            return ValueError("Negative index is not yet supported")
+
+        new_node = Node(value)
+
+        # If the Linked List is null, simply put it there
+        if self.head is None:
+            self.head = new_node
+            return None
+
+        # If the Linked List isn't null (cuz it didn't return at the prev `if`)
+
+        if index == 0:
+            # move the `head` to the new node's `next`
+            #   head(10, None) | new(20, None) => (20, head(10, None))
+            #   you don't need to care about the rest of el since it's LinkedL
+            new_node.next = self.head
+
+            # set the new node as `head`
+            #   head(10, None) -> head(20, 10)
+            self.head = new_node
+
+        else:
+            # prepare to loop through this whole thing (Linked List)
+            current_node = self.head
+            current_index = 0
+
+            while current_node:
+
+                # find the node that before the node at the index (c -> c.next)
+                if current_index == index - 1:
+
+                    # there's only two nodes are involved
+                    #   target_node         save the node we'll replace
+                    #   current_node.next   precisely the node we'll replace
+                    #   new_node.next       put saved node into new's `next`
+                    target_node = current_node.next
+                    current_node.next = new_node
+                    new_node.next = target_node
+
+                    return None
+
+                current_index += 1
+                current_node = current_node.next
+
+        raise IndexError("SinglyLinkedList assignment index out of range")
+
+    def pop(self, index):
+        """
+        O(N)
+        """
+        if index < 0:
+            raise ValueError("Negative index is not yet supported")
+
+        # if the index is 0, i.e. the `head` node
+        if index == 0:
+            deleted_value = self.head.value
+            self.head = self.head.next
+            return deleted_value
+
+        # if the index is non-zero, we'll need to set `head` properly!
+        else:
+            # set the node to the "beginning" (excluding the 1st [0])
+            previous_node = self.head
+            current_node = self.head.next
+
+            # set the index to its corresponding place (0 is catched by `if`)
+            current_index = 1
+
+            while current_node:
+                if current_index == index:
+                    # "delete" elem by shifting the elements forward
+                    previous_node.next = current_node.next
+
+                    # after "delete", return the element
+                    deleted_value = current_node.value
+                    return deleted_value
+
+                # "delete" elem by shifting the elements forward
+                previous_node = current_node
+
+                # move towards the end (the last elem whose `next` is None)
+                current_node = current_node.next
+                current_index += 1
+
+        raise IndexError("SinglyLinkedList deletion index out of range")
+
     def reverse(self):
         """
         O(N)
         """
-        pass
+        current_node = self.head
+        previous_node = None
 
-    def insert_before(self):
-        pass
+        while current_node:
+            next_node = current_node.next
 
-    def pop(self):
-        pass
+            current_node.next = previous_node
+            previous_node = current_node
+
+            current_node = next_node
+
+        self.head = previous_node
 
 
 def main():
+    """
+    Understanding of different parts
+    - method `reverse`  meh
+    - the rest of them  100%
+    """
     pass
 
 
